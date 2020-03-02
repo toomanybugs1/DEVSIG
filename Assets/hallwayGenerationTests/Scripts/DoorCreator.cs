ee29812f6c02c039fb7e8b606e57484e84fbd52c
James <jamestompkins@hotmail.co.uk>
2020-03-02 13:30:04 -0600

    Added room generation tests
    -room generation script
    -sample scene with number based generation
    -imported textMesh assets

---
+++ Assets/hallwayGenerationTests/Scripts/DoorCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCreator : MonoBehaviour {
+    [SerializeField] GameObject doorPrefab;
+    [SerializeField] int doorAmount;
+    float radius;
+
+    void Start() {
+        radius = (doorAmount / 2) + 5;
+
+        for (int i = 0; i < doorAmount; i++) {
+            float angle = (Mathf.PI * 2 / doorAmount) * i;
+            Vector3 pos = new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
+            GameObject newInstance = Instantiate(doorPrefab, pos, Quaternion.identity, transform);
+            newInstance.transform.LookAt(transform.position);
+        }
+    }
+}
---
+++ Assets/hallwayGenerationTests/Scripts/DoorInteractable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteractable : IInteractable {
+
+    [SerializeField] BoxCollider collider;
+
+    public void OnInteract() {
+
+    }
+}
