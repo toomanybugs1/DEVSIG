30f3dfc294ef25ec3c58adf893548e89991e928d
James <jamestompkins@hotmail.co.uk>
2020-03-04 22:30:16 -0600

    Added a bunch to the start room (door room)
    Doors are now generated and labeled based off of scene names. Scenes are loaded by a players raycast.

---
+++ Assets/Scripts/FirstPersonRaycast.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonRaycast : MonoBehaviour
+{
+    Camera cam;
+
+    private void Start() {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            RaycastHit hit;
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+
+            if (Physics.Raycast(ray, out hit)) {
+                Transform objectHit = hit.transform;
+                SceneLoader sceneLoader = hit.transform.GetComponent<SceneLoader>();
+
+                if (sceneLoader) {
+                    sceneLoader.loadScene();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StartRoom/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string scene;
+
+    public void loadScene() {
+        SceneManager.LoadScene(scene);
+    }
+}
