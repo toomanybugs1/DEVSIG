c6ef9bdcf39ff70e176a1ea4983fb124af23eba1
James <jamestompkins@hotmail.co.uk>
2020-03-05 20:05:35 -0600

    Switched to scriptableObject
    now we can have custom data for the level such as a floating point difficulty to be used later for images

---
+++ Assets/Scenes/LevelScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(menuName = "LevelData")]
+public class LevelScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    public SceneAsset scene;
+    [Range(0,1)]
+    public float difficulty;
+}
--- Assets/Scripts/StartRoom/DoorCreator.cs
+++ Assets/Scripts/StartRoom/DoorCreator.cs
@@ -1,15 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.IO;
+﻿using UnityEngine;
 using UnityEditor;
-using UnityEngine.UI;
 using TMPro;
 
 public class DoorCreator : MonoBehaviour {
     [SerializeField] GameObject doorPrefab;
-    [SerializeField] SceneAsset[] scenes;
+    [SerializeField] LevelScriptableObject[] scenes;
     float radius;
 
     void Start() {
@@ -23,31 +18,10 @@ public class DoorCreator : MonoBehaviour {
 
             newInstance.transform.LookAt(transform.position);
 
-            SceneAsset scene = scenes[i];
+            LevelScriptableObject level = scenes[i];
 
-            //we can keep track of difficulty with the first character of the scene
-            //this way we don't tell the user the name of each scene, just it's difficulty
-            //we will likely replace this with a graphic
-            char diffChar = scene.name[0];
-            string difficulty;
-
-            switch (diffChar) {
-                case 'E':
-                    difficulty = "Easy";
-                    break;
-                case 'M':
-                    difficulty = "Medium";
-                    break;
-                case 'H':
-                    difficulty = "Hard";
-                    break;
-                default:
-                    difficulty = "Easy";
-                    break;
-            }
-
-            newInstance.GetComponentInChildren<TMP_Text>().text = difficulty;
-            newInstance.GetComponentInChildren<SceneLoader>().setScene(scene.name);
+            newInstance.GetComponentInChildren<TMP_Text>().text = "" + level.difficulty;
+            newInstance.GetComponentInChildren<SceneLoader>().setScene(level.scene.name);
         }
     }
 }
