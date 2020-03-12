e9b1c14cf85956e3c6bc1ea44199257e4c0c9fb9
James Tompkins <jamestompkins@hotmail.co.uk>
2020-03-12 17:37:11 -0500

    swapped idea to defence game
    created desert island
    navigating enemy

---
+++ Assets/CrabController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CrabController : MonoBehaviour
+{
+    Transform goal;
+    NavMeshAgent agent;
+
+    void Start() {
+        goal = FindObjectOfType<FirstPersonAIO>().transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        agent.destination = goal.position;
+    }
+}
---
+++ Assets/Games/DesertIslandDefender/Scripts/PlaneGizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneGizmo : MonoBehaviour
+{
+    [SerializeField] bool showing = true;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        if (showing) {
+            Gizmos.DrawWireCube(transform.position, transform.localScale);
+        }
+    }
+}
